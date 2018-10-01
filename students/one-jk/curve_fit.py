05af6890893946416baf7ad6d5797c3db64100c5
Jackson Keppen <keppenj@oregonstate.edu>
2018-10-01 15:31:57 -0700

    Adding curve fit. Trial run

---
+++ students/one-jk/curve_fit.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.polynomial.polynomial as poly
+
+plt.style.use('seaborn-whitegrid')
+
+data = np.loadtxt("data/data_p.txt")
+
+print(data)
+#fig = plt.figure()
+#ax = plt.axes()
+
+
+
+#z = poly.polyfit(v, e, 4)
+#print(z)
+#y = poly.polyfit(v, e, 6)
+
+#Come back and use scripy.optimize.curve
+#zspace = np.linspace(3,10,30)
+#ffit = poly.Polynomial(z)
+#yfit = poly.Polynomial(y)
+
+#print(v)
+#print(zspace)
+
+
+#plt.plot(v, e, '.')
+#plt.plot(zspace, ffit(zspace), '-')
+#plt.plot(zspace, yfit(zspace), '--')
+
+#mini = np.amin(ffit(zspace))
+
+#rint(mini)
+#plt.plot(xp, v, '-')
+#E(v) = E_0 + (B_0 * (v/Bprime)((1/(Bprime-1)*((V_0/v)^Bprime) +1) - (B_0*V_0/(Bprime-1))
+
+#plt.show()
+
+print("Hello World")
