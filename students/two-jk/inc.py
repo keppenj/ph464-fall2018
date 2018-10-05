d837350e39aeccd542779113074e0dea8f4e2f5b
Jackson Keppen <keppenj@oregonstate.edu>
2018-10-05 14:33:36 -0700

    Just trying to keep up with what is going on in class. I am failing at that.

---
+++ students/two-jk/inc.py
@@ -0,0 +1,9 @@
+#inc.py
+def inc(x):
+  return x + 2
+
+#if __name__ = '__main__':
+#
+#    #test
+#    if inc(3) != 4:
+#        print ('>>> Error in inc()')
---
+++ students/two-jk/inc_test.py
@@ -0,0 +1,4 @@
+#test_inc.py
+from inc.py import inc
+
+def(test)
